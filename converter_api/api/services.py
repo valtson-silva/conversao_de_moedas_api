36c18530059f29227a8c6bc64622dfa2440c4ead
Váltson Araújo <valtson88@gmail.com>
2025-02-15 14:30:03 -0300

    Projeto atualizado

--- converter_api/api/services.py
+++ converter_api/api/services.py
@@ -13,7 +13,6 @@ def convert_currency_crypto(from_currency, to_currency, amount):
     from_currency = names["from_currency"]
     to_currency = names["to_currency"]   
      
-    # Declaro as variáveis como none
     coin_origin = None
     coin_destiny = None          
     
@@ -21,9 +20,9 @@ def convert_currency_crypto(from_currency, to_currency, amount):
     cache_key = f"{from_currency}_{to_currency}"
     # Tenta obter os dados do cache
     cache_json = cache.get(cache_key)
-    # Verifica se os dados foram obtidos
+    
     if cache_json:
-        # Converte para dicionário
+        
         cache_dict = json.loads(cache_json)
         # Tenta obter os valores do dicionário
         coin_origin = cache_dict["from_currency"]
@@ -35,13 +34,13 @@ def convert_currency_crypto(from_currency, to_currency, amount):
         response = requests.get("https://api.coingecko.com/api/v3/simple/price", params={"ids": f"{from_currency},{to_currency}", "vs_currencies": "usd"})
         
         if response.status_code == 200:
-            # Transforma os dados em json
+            
             data = response.json()
             
             # Obtém os valores das criptos em dólar
             coin_origin = data[from_currency]["usd"]
             coin_destiny = data[to_currency]["usd"]
-            # Dicionário para cache
+            
             data_cache = {"from_currency": coin_origin, "to_currency": coin_destiny}
             # Salva no cache por 1 hora
             cache.set(cache_key, json.dumps(data_cache), timeout=3600)
@@ -59,31 +58,31 @@ def convert_currency_coin(from_currency, to_currency, amount):
     
     # Verifica se o amount é válido
     if float(amount) < 0.01:
-        # Retorna error
+        
         raise ValidationError("Valor da moeda inválido.")
     else:
         # Chave única do cache
         cache_key = f"{from_currency}_{to_currency}"
-        # Tenta pegar do cache
+        
         conversion_rate = cache.get(cache_key)
-        # Verifica se conseguiu pegar do cache a taxa
+        
         if not conversion_rate:
             # Chave da API externa
             API_KEY = config("API_KEY")
-            # url
+            
             url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{from_currency}/{to_currency}"
             # Faz a requisição get pra api externa
             response = requests.get(url)
-            # Verifica se o status foi 200
+            
             if response.status_code == 200:
-                # Transforma a resposta da api em json
+                
                 data = response.json()
-                # Obtém a taxa de conversão
+                
                 conversion_rate = data["conversion_rate"]
                 # Salva no cache por 1 hora
                 cache.set(cache_key, conversion_rate, timeout=3600)
              
-        # Verifca se a taxa existe
+        
         if conversion_rate:
             # Retorna o valor da conversão e a taxa 
             return {"convert_amount": float(amount) * conversion_rate, "conversion_rate": conversion_rate}
